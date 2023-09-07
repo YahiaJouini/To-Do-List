be796c1dda632e08feb321ca2b356364ea165614
YahiaJouini <jouiniyahya117@gmail.com>
2023-09-07 16:10:15 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,69 @@
+const task=document.querySelector("input[type='text']");
+const add=document.querySelector('.add');
+const container=document.querySelector('.container');
+
+function addTask(e){
+    let taskNumber=container.childElementCount-2;
+    if(taskNumber>9){
+        task.value=""
+        alert('Finish your current tasks first ')
+    }else if(task.value==""){
+        alert('Enter a valid task')
+    }else{
+    let bigDiv=document.createElement('div');
+    bigDiv.classList.add('tasks');
+
+    let insideDiv=document.createElement('div');
+    insideDiv.classList.add("task");
+
+    let img=document.createElement('img');
+    img.src="images/unchecked.png";
+    let para=document.createElement('p');
+    para.appendChild(document.createTextNode(task.value));
+
+
+    insideDiv.appendChild(img)
+    insideDiv.append(para)
+
+    let btn=document.createElement('button');
+    btn.classList.add('dlt');
+    btn.innerHTML='<i class="fa fa-close"></i>';
+    bigDiv.appendChild(insideDiv);
+    bigDiv.appendChild(btn);
+    container.appendChild(bigDiv);
+    task.value=""
+}}
+add.addEventListener("click",addTask);
+
+container.addEventListener('click',(e) =>{
+    let target=e.target
+    if(target.tagName=='P' || target.tagName=="IMG" || target.classList.contains('task')){
+        if(target.tagName=='IMG'){
+            p=target.nextElementSibling;
+        }else if(target.tagName=='DIV'){
+            p=target.lastChild;
+        }else{
+            p=target
+        }
+        if(p.classList.contains('done')){
+            p.classList.remove('done')
+            p.previousSibling.src="images/unchecked.png";
+        }else{
+            p.classList.add('done');
+            p.previousSibling.src="images/checked.png";
+        }
+    }
+})
+container.addEventListener('click',(e)=>{
+    let target=e.target
+    if(target.className=="dlt" || target.tagName=='I'){
+        if(target.parentElement.tagName=="BUTTON"){
+            div=target.parentElement.parentElement
+        }
+        else{
+            div=target.parentElement
+        }
+        if(confirm("Are you sure?")){
+        div.remove()
+    }}
+})
